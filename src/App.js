33546983dddf728fba61b8a5687303d43b6005ad
Ravil Ayupov <35985944+RexCloud@users.noreply.github.com>
2023-02-28 22:38:06 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
+import { Web3Button } from "@thirdweb-dev/react";
+
 
 export default function Home() {
   return (
     <div className="container">
       <main className="main">
         <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+          Scroll Alpha Test USDC Faucet
         </h1>
 
         <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
+          Claim your test USDC tokens
         </p>
 
-        <div className="connect">
-          <ConnectWallet />
-        </div>
+        <p>
+          Switch to Scroll Alpha Testnet before claiming
+        </p>
+        <Web3Button
+          contractAddress="0xeF71Ddc12Bac8A2ba0b9068b368189FFa2628942"
+          contractAbi={[{"type":"constructor","stateMutability":"nonpayable","inputs":[]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"claim","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"claimAmount","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"owner","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"bool","name":"","internalType":"bool"}],"name":"received","inputs":[{"type":"address","name":"","internalType":"address"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"setClaimAmount","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"usdc","inputs":[]}]}
+          action={(contract) => contract.call("claim")}
+          onError={() => alert("Error: Already received faucet / Insufficient funds")}
+        >
+          Claim
+        </Web3Button>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        <p>Trade on Uniswap:</p>
+        <a href="https://uniswap-v3.scroll.io/#/swap?outputCurrency=0xA0D71B9877f44C744546D649147E3F1e70a93760">USDC/WETH</a>
+        
       </main>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,23 +3,111 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+
 import "./styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "scroll-alpha-testnet";
+
+const goerliChain = {
+  "name": "Goerli",
+  "title": "Ethereum Testnet Goerli",
+  "chain": "ETH",
+  "rpc": [
+    "https://goerli.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}",
+    "https://goerli.infura.io/v3/${INFURA_API_KEY}",
+    "wss://goerli.infura.io/v3/${INFURA_API_KEY}",
+    "https://rpc.goerli.mudit.blog/"
+  ],
+  "faucets": [
+    "https://faucet.goerli.mudit.blog",
+    "https://goerli-faucet.slock.it?address=${ADDRESS}",
+    "http://fauceth.komputing.org?chain=5&address=${ADDRESS}"
+  ],
+  "nativeCurrency": {
+    "name": "Goerli Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "infoURL": "https://goerli.net/#about",
+  "shortName": "gor",
+  "chainId": 5,
+  "networkId": 5,
+  "ens": {
+    "registry": "0x112234455c3a32fd11230c42e7bccd4a84e02010"
+  },
+  "explorers": [
+    {
+      "name": "etherscan-goerli",
+      "url": "https://goerli.etherscan.io",
+      "standard": "EIP3091"
+    }
+  ],
+  "icon": {
+    "url": "ipfs://QmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9/ethereum/512.png",
+    "height": 512,
+    "width": 512,
+    "format": "png",
+    "sizes": [
+      512,
+      256,
+      128,
+      64,
+      32,
+      16
+    ]
+  },
+  "testnet": true,
+  "slug": "goerli"
+}
+
+const scrollChainL2 = {
+  "name": "Scroll Alpha Testnet",
+  "chain": "ETH",
+  "status": "incubating",
+  "rpc": [
+    "https://scroll-alpha-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://alpha-rpc.scroll.io/l2"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "infoURL": "https://scroll.io",
+  "shortName": "scr-alpha",
+  "chainId": 534353,
+  "networkId": 534353,
+  "explorers": [
+    {
+      "name": "Scroll Alpha Testnet Block Explorer",
+      "url": "https://blockscout.scroll.io",
+      "standard": "EIP3091"
+    }
+  ],
+  "parent": {
+    "type": "L2",
+    "chain": "eip155-5",
+    "bridges": []
+  },
+  "testnet": true,
+  "slug": "scroll-alpha-testnet"
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={activeChain} supportedChains={[goerliChain, scrollChainL2]}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
 );
-
+// console.log(chains);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
