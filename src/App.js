4672f93fdb3e55dde8fa2d1be923730efd809c22
Ravil Ayupov <35985944+RexCloud@users.noreply.github.com>
2023-08-18 16:01:02 +0000

    Updated for Scroll Sepolia

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <main className="main">
         <h1 className="title">
-          Scroll Alpha Test USDC Faucet
+          Scroll Sepolia Test USDC Faucet
         </h1>
 
         <p className="description">
@@ -15,10 +15,10 @@ export default function Home() {
         </p>
 
         <p>
-          Switch to Scroll Alpha Testnet before claiming
+          Switch to Scroll Sepolia Testnet before claiming
         </p>
         <Web3Button
-          contractAddress="0xeF71Ddc12Bac8A2ba0b9068b368189FFa2628942"
+          contractAddress="0xA9e6eF1F9533f43badA125CbcDFA5eeA35BE4475"
           contractAbi={[{"type":"constructor","stateMutability":"nonpayable","inputs":[]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"claim","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"claimAmount","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"owner","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"bool","name":"","internalType":"bool"}],"name":"received","inputs":[{"type":"address","name":"","internalType":"address"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"setClaimAmount","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"usdc","inputs":[]}]}
           action={(contract) => contract.call("claim")}
           onError={() => alert("Error: Already received faucet / Insufficient funds")}
@@ -27,7 +27,7 @@ export default function Home() {
         </Web3Button>
 
         <p>Trade on Uniswap:</p>
-        <a href="https://uniswap-v3.scroll.io/#/swap?outputCurrency=0xA0D71B9877f44C744546D649147E3F1e70a93760">USDC/WETH</a>
+        <a href="https://uniswap-v3.scroll.io/#/swap?outputCurrency=0x690000EF01deCE82d837B5fAa2719AE47b156697">USDC/WETH</a>
         
       </main>
     </div>
--- src/index.js
+++ src/index.js
@@ -9,68 +9,56 @@ import "./styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "scroll-alpha-testnet";
+const activeChain = "scroll-sepolia-testnet";
 
-const goerliChain = {
-  "name": "Goerli",
-  "title": "Ethereum Testnet Goerli",
+const sepoliaChain = {
+  "name": "Sepolia",
+  "title": "Ethereum Testnet Sepolia",
   "chain": "ETH",
   "rpc": [
-    "https://goerli.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
-    "https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}",
-    "https://goerli.infura.io/v3/${INFURA_API_KEY}",
-    "wss://goerli.infura.io/v3/${INFURA_API_KEY}",
-    "https://rpc.goerli.mudit.blog/"
+    "https://sepolia.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://sepolia.infura.io/v3/${INFURA_API_KEY}",
+    "wss://sepolia.infura.io/v3/${INFURA_API_KEY}",
+    "https://rpc.sepolia.org",
+    "https://rpc2.sepolia.org",
+    "https://rpc-sepolia.rockx.com",
+    "https://rpc.sepolia.ethpandaops.io"
   ],
   "faucets": [
-    "https://faucet.goerli.mudit.blog",
-    "https://goerli-faucet.slock.it?address=${ADDRESS}",
-    "http://fauceth.komputing.org?chain=5&address=${ADDRESS}"
+    "http://fauceth.komputing.org?chain=11155111&address=${ADDRESS}"
   ],
   "nativeCurrency": {
-    "name": "Goerli Ether",
+    "name": "Sepolia Ether",
     "symbol": "ETH",
     "decimals": 18
   },
-  "infoURL": "https://goerli.net/#about",
-  "shortName": "gor",
-  "chainId": 5,
-  "networkId": 5,
-  "ens": {
-    "registry": "0x112234455c3a32fd11230c42e7bccd4a84e02010"
-  },
+  "infoURL": "https://sepolia.otterscan.io",
+  "shortName": "sep",
+  "chainId": 11155111,
+  "networkId": 11155111,
   "explorers": [
     {
-      "name": "etherscan-goerli",
-      "url": "https://goerli.etherscan.io",
+      "name": "etherscan-sepolia",
+      "url": "https://sepolia.etherscan.io",
+      "standard": "EIP3091"
+    },
+    {
+      "name": "otterscan-sepolia",
+      "url": "https://sepolia.otterscan.io",
       "standard": "EIP3091"
     }
   ],
-  "icon": {
-    "url": "ipfs://QmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9/ethereum/512.png",
-    "height": 512,
-    "width": 512,
-    "format": "png",
-    "sizes": [
-      512,
-      256,
-      128,
-      64,
-      32,
-      16
-    ]
-  },
   "testnet": true,
-  "slug": "goerli"
-}
+  "slug": "sepolia"
+};
 
 const scrollChainL2 = {
-  "name": "Scroll Alpha Testnet",
+  "name": "Scroll Sepolia Testnet",
   "chain": "ETH",
-  "status": "incubating",
+  "status": "active",
   "rpc": [
-    "https://scroll-alpha-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
-    "https://alpha-rpc.scroll.io/l2"
+    "https://scroll-sepolia-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://sepolia-rpc.scroll.io/"
   ],
   "faucets": [],
   "nativeCurrency": {
@@ -79,30 +67,34 @@ const scrollChainL2 = {
     "decimals": 18
   },
   "infoURL": "https://scroll.io",
-  "shortName": "scr-alpha",
-  "chainId": 534353,
-  "networkId": 534353,
+  "shortName": "scr-sepolia",
+  "chainId": 534351,
+  "networkId": 534351,
   "explorers": [
     {
-      "name": "Scroll Alpha Testnet Block Explorer",
-      "url": "https://blockscout.scroll.io",
+      "name": "Scroll Sepolia Testnet Block Explorer",
+      "url": "https://sepolia-blockscout.scroll.io",
       "standard": "EIP3091"
     }
   ],
   "parent": {
     "type": "L2",
-    "chain": "eip155-5",
-    "bridges": []
+    "chain": "eip155-11155111",
+    "bridges": [
+      {
+        "url": "https://scroll.io/bridge"
+      }
+    ]
   },
   "testnet": true,
-  "slug": "scroll-alpha-testnet"
+  "slug": "scroll-sepolia-testnet"
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain} supportedChains={[goerliChain, scrollChainL2]}>
+    <ThirdwebProvider activeChain={activeChain} supportedChains={[sepoliaChain, scrollChainL2]}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
